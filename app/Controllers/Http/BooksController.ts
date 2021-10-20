1678f9535df7900933eb5ca36d43724b510ca085
Artur Ligieri Nunes <art.nunes@gmail.com>
2021-10-19 21:06:19 -0300

    feat: create booksRepository

--- app/Controllers/Http/BooksController.ts
+++ app/Controllers/Http/BooksController.ts
@@ -1,12 +1,20 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { BookDTO } from 'App/Dto/BookDTO'
+import BooksRepository from 'App/Repositories/BooksRepository'
 
 export default class BooksController {
+  private readonly booksRepository: BooksRepository
+
+  constructor() {
+    this.booksRepository = new BooksRepository()
+  }
+
   public async index({}: HttpContextContract) {}
 
   public async create({ request, response }: HttpContextContract) {
     const book = request.body() as BookDTO
-    const books = this.addBook(book)
-    response.status(200).json(books)
+    const books = this.booksRepository.add(book)
+    response.status(201).json(books)
   }
 
   public async update({}: HttpContextContract) {}
--- app/Repositories/BooksRepository.ts
+++ app/Repositories/BooksRepository.ts
@@ -1,21 +1,19 @@
-/* eslint-disable @typescript-eslint/explicit-member-accessibility */
 import { BookDTO } from 'App/Dto/BookDTO'
 import { Book } from 'App/Models/Book'
 
+let books: Book[] = []
+let id = 0
 export default class BooksRepository {
-  books: Book[] = []
-  id = 0
-
   public add({ authors, image, publisher, title }: BookDTO): Book[] {
-    this.id += 1
+    id += 1
     const book: Book = {
-      id: this.id,
+      id: id,
       titulo: title,
       editora: publisher,
       foto: image,
       autores: authors,
     }
-    this.books.push(book)
-    return this.books
+    books.push(book)
+    return books
   }
 }
